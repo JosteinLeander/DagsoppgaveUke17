a9be45f0c8a49c888dece8d2d7b1a9a038667383
JosteinLeander <jlelangmo@gmail.com>
2023-04-24 21:45:29 +0200

    Snart ferdig

--- js.js
+++ js.js
@@ -6,9 +6,15 @@ function registrerNavn() {
     let navn = document.getElementById("navn").value;
     document.getElementById("velkommen").innerHTML = "Velkommen, vi ønsker deg alt som er godt, " + navn + "!";
 
-    localStorage.setItem()
+    localStorage.setItem(navn, navn);
+    let len = localStorage.length;
+    console.log(len);
     let navnene = "";
-    navnene += "<li>" + navn + "</li>";
+    for (let i = 0; i < len; i++) {
+        let key = localStorage.key(i);
+        navnene += "<li>" + localStorage[key] + "</li>";
+    }
+    
     
     document.getElementById("navnene").innerHTML = navnene;
 }
